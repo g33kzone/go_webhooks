c9a8e3bf21b1e49c09900eade58f445c7f66458d
Manish Raghunath Warang <manish_warang@infosys.com>
2019-03-25 00:39:36 +0530

    added go.mod and go.sum files

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
 
 func main() {
-	fmt.Print("Hello World !!")
+	router = gin.Default()
+	initializeRoutes()
+	router.Run(":8000")
+}
+
+func initializeRoutes() {
+	router.POST("/api", handleWebHooks)
+}
+
+func handleWebHooks(c *gin.Context) {
+	fmt.Print("Webhook received...")
+	c.String(http.StatusOK, "POST request received...")
 }
