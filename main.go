f522839bde1db6c51448c4d7549174b275dc86d6
Manish Warang <Manish_Warang@infosys.com>
2019-07-12 12:58:20 +0530

    updated source code for GCP cloudbuild

--- main.go
+++ main.go
@@ -1,27 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
 
 var router *gin.Engine
-
-func init() {
-	// Log as JSON instead of the default ASCII formatter.
-	log.SetFormatter(&log.JSONFormatter{})
-
-	// Output to stdout instead of the default stderr
-	// Can be any io.Writer, see below for File example
-	log.SetOutput(os.Stdout)
-
-	// Only log the warning severity or above.
-	log.SetLevel(log.InfoLevel)
-}
 
 func main() {
 	router = gin.Default()
@@ -30,29 +16,11 @@ func main() {
 }
 
 func initializeRoutes() {
-	router.POST("/api", handleWebHooks)
 	router.GET("/api", fetchHelloWorld)
 }
 
-func handleWebHooks(c *gin.Context) {
-	fmt.Println("Webhook received...")
-	log.Infoln("Test -  Info")
-	log.Warnln("Test - Warning")
-	log.WithFields(log.Fields{
-		"GCP Project":  "Test -  Project",
-		"Product Name": "Test - Product",
-		"size":         10}).Info("Test - Infoln")
-	log.Errorln("Test - Error")
-	c.String(http.StatusOK, "POST request received...")
-}
 func fetchHelloWorld(c *gin.Context) {
-	fmt.Println("received GET call...")
-	log.Infoln("Test -  Info")
-	log.Warnln("Test - Warning")
-	log.WithFields(log.Fields{
-		"GCP Project":  "Test -  Project",
-		"Product Name": "Test - Product",
-		"size":         10}).Info("Test - Infoln")
-	log.Errorln("Test - Error")
+	log.Info("GET API call received...")
+
 	c.JSON(http.StatusOK, "Hello World !!")
 }
